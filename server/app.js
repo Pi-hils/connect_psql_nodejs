4ddc7110cfbf6dde0c7318768d6e74d241433ebd
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-24 10:03:07 +0000

    added a new info

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const client = require('./database');
 (async ()=>{
  client.connect();
 const result = await client.query(`insert into info(name, email)
-  values($1,$2) RETURNING *` , ['Anita Serwaa','serwaa@hello.com']);
+  values($1,$2) RETURNING *` , ['Bella Nyarko','bella@hello.com']);
   console.log(result.rows);
   console.log(result.rowCount);
   client.end();
